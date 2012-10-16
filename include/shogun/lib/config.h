a5ace7aaa6ff528401eefc2b531bbe932164f9a6
Gunnar Raetsch <raetsch@cbio.mskcc.org>
2012-10-16 08:44:26 -0400

    created orphaned ubuntu-10.04 branch

---
+++ include/shogun/lib/config.h
@@ -0,0 +1,54 @@
+
+#define HAVE_HDF5 1
+
+#define HAVE_XML 1
+#define HAVE_LARGEFILE 1
+
+#define HAVE_LAPACK 1
+
+
+
+#define HAVE_PTHREAD 1
+
+
+
+#define HAVE_POWL 1
+#define HAVE_LGAMMAL 1
+#define HAVE_SQRTL 1
+#define HAVE_LOG2 1
+
+
+
+#define USE_SPINLOCKS 1
+
+#define USE_SHORTREAL_KERNELCACHE 1
+
+
+
+
+
+
+#define USE_BIGSTATES 1
+#define USE_HMMCACHE 1
+
+
+
+
+#define USE_SVMLIGHT 1
+
+
+
+
+
+#define USE_GZIP 1
+#define USE_BZIP2 1
+
+
+#define USE_REFERENCE_COUNTING 1
+#define OCTAVE_APIVERSION 45
+#define LINUX 1
+#define CONFIGURE_OPTIONS "configure options --prefix=/mnt/oqtansTools/oqtans_dep --interfaces=octave_static,python_modular"
+#define MACHINE "x86_64"
+#define COMPFLAGS_C	"-fPIC -g -Wall -Wno-unused-parameter -Wformat -Wformat-security -Wparentheses -Wshadow -O9 -fexpensive-optimizations -frerun-cse-after-loop -fcse-follow-jumps -finline-functions -fschedule-insns2 -fthread-jumps -fforce-addr -fstrength-reduce -funroll-loops -mfpmath=sse   -pthread"
+#define COMPFLAGS_CPP "-fPIC -g -Wall -Wno-unused-parameter -Wformat -Wformat-security -Wparentheses -Wshadow -Wno-deprecated -O9 -fexpensive-optimizations -frerun-cse-after-loop -fcse-follow-jumps -finline-functions -fschedule-insns2 -fthread-jumps -fforce-addr -fstrength-reduce -funroll-loops -mfpmath=sse   -pthread"
+#define LINKFLAGS " -Xlinker --no-undefined"
